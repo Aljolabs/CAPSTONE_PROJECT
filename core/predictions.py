1aa000306d0e3f71e9f6ec85be51193ddf0ef7cb
Aljolabs <aljoquindoza@gmail.com>
2025-11-17 18:39:20 +0800

    trying to do something like scoring system

--- core/predictions.py
+++ core/predictions.py
@@ -2,6 +2,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 
+def staff_cmputation(df, employee_id):
+    employee = df[df['employee_id'] == employee_id]
+
+    score = 0.4 * employee['quality'].values[0] * 0.3 * employee['punctuality'].values[0] * 0.2 * employee['behavior'].values[0] * 0.1 * employee['overall'].values[0]
+    return score
+
 def employeeSuggestion(df, features, target, new_employee):
     """This function creates a employee suggestion based
     on the given data. This function tries to suggests
